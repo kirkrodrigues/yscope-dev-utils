5653c8a4fd321bf229ab6b46a61abd69273c4b87
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-05-09 20:29:24 -0400

    Add validation of global variable naming.

--- linters/taskfile-lint/src/taskfile_lint/main.py
+++ linters/taskfile-lint/src/taskfile_lint/main.py
@@ -53,18 +53,44 @@ def main(argv=None):
     linting_failed = False
     if taskfile_path.suffix != ".yaml":
         print(f"{taskfile_path}: Taskfile must use the '.yaml' extension.", file=sys.stderr)
-        linting_failed = True
+        linting_failed |= True
 
     with open(taskfile_path, "r") as f:
         doc = yaml.safe_load(f)
 
-    if not _validate_task_attribute_order_for_all_tasks(taskfile_path, doc):
-        linting_failed = True
-
+    linting_failed |= _validate_global_variable_prefix(taskfile_path, doc)
+    linting_failed |= _validate_task_attribute_order_for_all_tasks(taskfile_path, doc)
     if linting_failed:
         return 1
 
     return 0
+
+
+def _validate_global_variable_prefix(
+    taskfile_path: Path, taskfile: Dict[str, Any]
+) -> bool:
+    """
+    Validates that every taskfile-level (global) variable is prefixed with `G_`.
+
+    :param taskfile_path:
+    :param taskfile:
+    :return: Whether all global variables have the correct prefix.
+    """
+
+    if "vars" not in taskfile:
+        # No global variables to validate
+        return True
+
+    linting_failed = False
+    for var_name in taskfile["vars"].keys():
+        if not var_name.startswith("G_"):
+            print(
+                f"{taskfile_path}: \"G_\" prefix missing from global variable '{var_name}'.",
+                file=sys.stderr,
+            )
+            linting_failed = True
+
+    return not linting_failed
 
 
 def _validate_task_attribute_order_for_all_tasks(
